fe4c5a2b1e62cd967f50e4657e5847860a7f4398
mubarak149 <mubarakharuna68@gmail.com>
2026-01-04 10:34:59 +0100

    create the students models

--- students/models.py
+++ students/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    user = models.OneToOneField('accounts.User', on_delete=models.CASCADE)
+    student_id = models.CharField(max_length=20, unique=True)
+    parent = models.ForeignKey('Parent', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name} - {self.student_id}"
+    
+class Parent(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name} - {self.phone_number}"
