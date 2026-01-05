4ea7240ea603d9e7a6071844d5550205bf5a0db4
mubarak149 <mubarakharuna68@gmail.com>
2026-01-05 09:39:20 +0100

    adding student CRUD

--- school_admin/views.py
+++ school_admin/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from accounts.forms import StudentUserForm
+from students.forms import StudentProfileForm, StudentClassForm
 
 # Create your views here.
 def admin_panel(request):
@@ -8,7 +10,15 @@ def admin_profile(request):
     return render(request, "school_admin/admin_profile.html")
 
 def manage_students(request):
-    return render(request, "school_admin/manage_students.html")
+# 🔥 GET request → send empty forms to frontend
+    user_form = StudentUserForm()
+    profile_form = StudentProfileForm()
+    class_form = StudentClassForm()
+    return render(request, "school_admin/manage_students.html", {
+        "user_form": user_form,
+        "profile_form": profile_form,
+        "class_form": class_form,
+    })
 
 def manage_teachers(request):
     return render(request, "school_admin/manage_teachers.html")
